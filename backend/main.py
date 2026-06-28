86619a4c77f3833775597844a98787559fc23ecd
mysahsrishkumar <mysahsrishkumar@gmail.com>
2026-06-29 01:07:34 +0530

    Test CORS

--- backend/main.py
+++ backend/main.py
@@ -20,12 +20,8 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        "http://localhost:5173",
-        "http://127.0.0.1:5173",
-        "https://ai-chat-app-frontend-icin.onrender.com",
-    ],
-    allow_credentials=True,
+    allow_origins=["*"],
+    allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
 )
